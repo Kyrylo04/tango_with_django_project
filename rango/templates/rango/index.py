3524d87b332d799bf6daab2ef3b152c92f31dffe
Kyrylo Petruska <2917357P@student.gla.ac.uk>
2025-02-14 07:13:36 +0000

    Initial commit with local files

---
+++ rango/templates/rango/index.py
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Rango</title>
+    </head>
+
+    <body>
+        <h1>Rango says...</h1>
+        <div>
+            hey there partner! <br />
+            <strong>{{ boldmessage }}</strong><br />
+        </div>
+        <img src="{% static 'images/rango.jpg' %}" alt="Picture of Rango" />
+    </body>  
+</html>
---
+++ rango/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def index(request):
+    # Construct a dictionary to pass to the template engine as its context.
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    return render(request, 'rango/index.html', context=context_dict)
