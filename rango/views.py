648204fd472aa1a28e59fe7b77e658f118d2d1b5
Kyrylo Petruska <2917357P@student.gla.ac.uk>
2025-02-14 14:36:17 +0000

    Task 7

--- rango/views.py
+++ rango/views.py
@@ -13,3 +13,18 @@ def category_detail(request, category_slug):
     }
 
     return render(request, 'rango/category_detail.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()  
+    
+    if request.method == 'POST':  
+        form = CategoryForm(request.POST)  
+        
+        if form.is_valid():
+            form.save(commit=True)  
+            return redirect('/rango/')  
+            
+        else:
+            print(form.errors)
+            
+    return render(request, 'rango/add_category.html', {'form': form})
